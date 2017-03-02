2be6bd912d8cee44370fb78ecda85280140dc9a0
Jaelson <jaelsonjunior99@gmail.com>
2017-03-01 21:09:40 -0300

    Add solution to #1.9

---
+++ matlab/tarefa1_9.m
@@ -0,0 +1,29 @@
+% 1.9. Aplicar filtros descritos na atividade
+function tarefa1_9()
+    assetsDir = ['..' filesep 'assets' filesep];
+    filePath = strcat(assetsDir, 'lenna.png');
+    
+    originalImage = imread(filePath);
+    filter1Image = filter1(originalImage);
+    filter2Image = filter2(originalImage);
+    
+        
+    % Ploting
+    figure();        
+    subplot(1,3,1), imshow(originalImage,[]), title('Original image')
+    subplot(1,3,2), imshow(filter1Image,[]), title('Filter1 image')
+    subplot(1,3,3), imshow(filter2Image,[]), title('Filter2 image')
+    
+end
+
+function output = filter1(input)
+    % MatLab shortcut
+    h = [ 0 -1 0 ; -1 5 -1 ; 0 -1 0 ];
+    output = imfilter(input,h);
+end
+
+function output = filter2(input)
+    % MatLab shortcut
+    h = [ 0 0 0 ; 0 1 0 ; 0 0 -1 ];
+    output = imfilter(input,h);
+end
