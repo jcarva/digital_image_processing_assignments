f8fb0c40ae335e90eb153e1e1f3f6b5c256ec7d2
Fernando Brito <email@fernandobrito.com>
2017-03-03 17:23:30 -0300

    [python] Attempts on task 1.1 and 1.2

--- python/task1_1.py
+++ python/task1_1.py
@@ -2,36 +2,53 @@
 # 1.1. Conversão RGB-YIQ-RGB
 # (cuidado com os limites de R, G e B!)
 
-import cv2
+import numpy as np
 import utils
 
 
 def main():
     image = utils.load_image('lenna.png')
 
-    blue, green, red = utils.split_channels(image)
+    yiq = _rgb2yiq(image)
+    back_to_original = _yiq2rgb(yiq)
 
-    utils.display_single_image('Blue Channel', blue)
-    utils.display_single_image('Green Channel', green)
-    utils.display_single_image('Red Channel', red)
-
-    back_to_original = utils.merge_channels(blue, green, red)
-
-    utils.display_single_image('Merged', back_to_original)
+    utils.display_single_image('Original', image)
+    utils.display_single_image('YIQ', yiq)
+    utils.display_single_image('Back to Original', back_to_original)
 
     utils.wait_key_and_destroy_windows()
 
 
-# From https://www.mathworks.com/help/images/ref/ntsc2rgb.html
-def _yiq2rgb(image):
-    conversion = [[0.299, 0.587, 0.114],
-             [0.596, -0.274, -0.322],
-             [0.211, -0.523, 0.312]]
-
-
-# From https://www.mathworks.com/help/images/ref/rgb2ntsc.html
 def _rgb2yiq(image):
-    conversion = [1]
+    output = np.copy(image)
+    blue, green, red = utils.split_channels(image)
+
+    # From https://www.mathworks.com/help/images/ref/rgb2ntsc.html
+    # Y = 0.299 R + 0.587 G + 0.114 B
+    # I = 0.596 R - 0.274 G - 0.322 B
+    # Q = 0.211 R - 0.523 G + 0.312 B
+
+    output[:, :, 0] = 0.299 * red + 0.587 * green + 0.114 * blue
+    output[:, :, 1] = 0.596 * red - 0.274 * green - 0.322 * blue
+    output[:, :, 2] = 0.211 * red - 0.523 * green + 0.312 * blue
+
+    return output
+
+
+def _yiq2rgb(image):
+    output = np.copy(image)
+    y, i, q = utils.split_channels(image)
+
+    # From https://www.mathworks.com/help/images/ref/ntsc2rgb.html
+    # R = 1 Y  + 0.956 I + 0.621 Q
+    # G = 1 Y  - 0.272 I - 0.647 Q
+    # B = 1 Y  - 1.106 I + 1.703 Q
+
+    output[:, :, 2] = y + 0.956 * i + 0.621 * q
+    output[:, :, 1] = y - 0.272 * i - 0.647 * q
+    output[:, :, 0] = y - 1.106 * i + 1.703 * q
+
+    return output
 
 
 if __name__ == "__main__":
