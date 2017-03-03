a27176d96c3223cf8fd2967f1ff483b91e46d912
Fernando Brito <email@fernandobrito.com>
2017-03-03 15:54:37 -0300

    [python] Create function to separate channels

--- python/task1_1.py
+++ python/task1_1.py
@@ -5,6 +5,28 @@
 import cv2
 import utils
 
-img = utils.load_image('lenna.png')
 
-utils.display_single_image('Lenna', img)
+def main():
+    image = utils.load_image('lenna.png')
+
+    blue, green, red = utils.split_channels(image)
+
+    utils.display_single_image('Blue Channel', blue)
+    utils.display_single_image('Green Channel', green)
+    utils.display_single_image('Red Channel', red)
+
+    utils.wait_key_and_destroy_windows()
+
+
+# From https://www.mathworks.com/help/images/ref/ntsc2rgb.html
+def _yiq2rgb(image):
+    conversion = [[0.299, 0.587, 0.114],
+             [0.596, -0.274, -0.322],
+             [0.211, -0.523, 0.312]]
+
+
+# From https://www.mathworks.com/help/images/ref/rgb2ntsc.html
+def _rgb2yiq(image):
+    conversion = [1]
+
+if __name__ == "__main__": main()
--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 
 def load_image(filename):
     return cv2.imread('../assets/' + filename)
@@ -7,5 +7,14 @@ def load_image(filename):
 
 def display_single_image(title, image):
     cv2.imshow(title, image)
+
+def wait_key_and_destroy_windows():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+'''
+Return B, G, R channels
+'''
+def split_channels(image):
+    return image[:,:,0], image[:,:,1], image[:,:,2]
