b123a1ca0e7de88d42c7ee1fadfa46af487db120
Jaelson <jaelsonjunior99@gmail.com>
2017-03-03 15:31:25 -0300

    Create display_multiple_images function

--- python/utils.py
+++ python/utils.py
@@ -9,3 +9,14 @@ def display_single_image(title, image):
     cv2.imshow(title, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def display_multiple_images(titles, images):
+    if len(titles) != len(images):
+        print("The length of titles and images are not compatible")
+        return False
+    else:
+        for i in range(0, len(images)):
+            cv2.imshow(titles[i], images[i])
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
