4decdbf5af919709db70c9775412677078f78798
Jaelson <jaelsonjunior99@gmail.com>
2017-03-05 20:54:32 -0300

    [matlab] Update solution for #1.8

--- matlab/task1_8.m
+++ matlab/task1_8.m
@@ -4,21 +4,31 @@ function task1_8()
     filePath = strcat(assetsDir, 'lenna.png');
     
     originalImage = imread(filePath);
-    sobelImage = sobelFilter(originalImage);
+    sobel_X_Image = sobel_X_Filter(originalImage);
+    sobel_Y_Image = sobel_Y_Filter(originalImage);
     laplacianImage = laplacianFilter(originalImage, 0.5);
             
     % Ploting
-    figure();        
-    subplot(1,3,1), imshow(originalImage,[]), title('Original image')
-    subplot(1,3,2), imshow(sobelImage,[]), title('Sobel image')
-    subplot(1,3,3), imshow(laplacianImage,[]), title('Laplacian image')
+    figure();
+    subplot(2,4,2), imshow(originalImage,[]), title('Original image')
+    subplot(2,4,3), imshow(laplacianImage,[]), title('Laplacian image')
+    
+
+    subplot(2,4,6), imshow(sobel_X_Image,[]), title('Sobel X image')
+    subplot(2,4,7), imshow(sobel_Y_Image,[]), title('Sobel Y image')
     
 end
 
-function output = sobelFilter(input)
+function output = sobel_X_Filter(input)
     % MatLab shortcut
     h = fspecial('sobel');
     output = imfilter(input, h);
+end
+
+function output = sobel_Y_Filter(input)
+    % MatLab shortcut
+    h = fspecial('sobel');
+    output = imfilter(input, h');
 end
 
 function output = laplacianFilter(input, alpha)
