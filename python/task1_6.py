72e701ad5240e082f962ba3cac88e951861ace67
Fernando Brito <email@fernandobrito.com>
2017-03-05 15:43:23 -0300

    [python] Improve solution of task 1.6

--- python/task1_6.py
+++ python/task1_6.py
@@ -17,23 +17,24 @@ def main():
 
     utils.wait_key_and_destroy_windows()
 
+
 def _segment_y_mean(image):
     m = np.mean(image)
-    print "M: %d" % m
-    return _segment_y(image, m)    
+    print "Mean: %d" % m
+    return _segment_y(image, m)
+
 
 def _segment_y(image, m):
-    #output = np.empty_like(image)
-    output = (image > m).astype(int)*256
-    print output
+    output = np.copy(image)
+    output[output > m] = 255
+
     return output
+
 
 def _rbg2gray(image):
     # https://en.wikipedia.org/wiki/Grayscale
     # Y ′ = 0.299 R ′ + 0.587 G ′ + 0.114 B
-    
-    #output = np.empty_like(image[:,:,2])
-    output = np.zeros(image.shape[0:2], dtype='uint8')
+
     blue, green, red = utils.split_channels(image)
     output = 0.299 * red + 0.587 * green + 0.114 * blue
 
