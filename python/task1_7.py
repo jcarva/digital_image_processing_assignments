f8e3cb5b952e489342e338f5c89186807577cda5
Jaelson <jaelsonjunior99@gmail.com>
2017-03-06 13:20:43 -0300

    [python] Complete solution to #1.7

--- python/task1_7.py
+++ python/task1_7.py
@@ -9,13 +9,13 @@ def main():
     image = utils.load_image('lenna.png')
 
     average_image = filter.apply_kernel(image, utils.average_kernel(9))
-
-    # TODO add median
+    median_image = filter.median_filter(image, 9)
 
     utils.display_single_image('Original', image)
-    utils.display_multiple_images(['Average'], [average_image])
+    utils.display_multiple_images(['Average', 'Median'], [average_image, median_image])
 
     utils.wait_key_and_destroy_windows()
+
 
 if __name__ == "__main__":
     main()
