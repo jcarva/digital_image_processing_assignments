d9d28b22c981d2833ed7ac1b6a44c9336b983474
Jaelson <jaelsonjunior99@gmail.com>
2017-03-05 23:30:05 -0300

    Fix formatting

--- python/task1_7.py
+++ python/task1_7.py
@@ -8,15 +8,15 @@ import utils
 def main():
     image = utils.load_image('lenna.png')
 
-    mean_image = utils.image_filter(image, _mean_kernel(9))
+    average_image = utils.image_filter(image, _average_kernel(9))
 
     utils.display_single_image('Original', image)
-    utils.display_multiple_images(['Mean'], [mean_image])
+    utils.display_multiple_images(['Average'], [average_image])
 
     utils.wait_key_and_destroy_windows()
 
 
-def _mean_kernel(kernel_size):
+def _average_kernel(kernel_size):
     return np.full((kernel_size, kernel_size), 1/float(kernel_size * kernel_size))
 
 if __name__ == "__main__":
--- python/task2_0.py
+++ python/task2_0.py
@@ -16,7 +16,7 @@ def main():
     utils.display_single_image('Salt&Pepper Noise', sp_image)
 
     filter1 = utils.image_filter(sp_image, np.full((3, 3), 1/9.0))
-    utils.display_single_image('Mean Filter', filter1)
+    utils.display_single_image('Average Filter', filter1)
 
     # filter2 = _filter(sp_image, [[0.0, 0.125, 0.0], [0.125, 0.25, 0.125], [0.0, 0.125, 0.0]])
     # utils.display_single_image('Median Filter', filter2)
