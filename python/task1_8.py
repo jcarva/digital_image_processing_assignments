50c906dfa44630b996d22b1d369b8de3bbf0942f
Fernando Brito <email@fernandobrito.com>
2017-03-07 14:58:41 -0300

    [python] Add Sobel addition

--- python/task1_8.py
+++ python/task1_8.py
@@ -12,9 +12,12 @@ def main():
     sobel_x_image = filter.apply_kernel(image, [[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     sobel_y_image = filter.apply_kernel(image, [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 
+    sobel_addition = sobel_x_image + sobel_y_image
+    utils.fit_matrix_in_interval(sobel_addition)
+
     utils.display_single_image('Original', image)
-    utils.display_multiple_images(['Sobel_X Lenna', 'Sobel_Y Lenna', 'Laplacian Lenna'],
-                                  [sobel_x_image, sobel_y_image, laplacian_image])
+    utils.display_multiple_images(['Sobel_X Lenna', 'Sobel_Y Lenna', 'Sobel X + Y', 'Laplacian Lenna'],
+                                  [sobel_x_image, sobel_y_image, sobel_addition, laplacian_image])
 
     utils.wait_key_and_destroy_windows()
 
