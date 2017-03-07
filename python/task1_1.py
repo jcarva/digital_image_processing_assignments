dbf20020b1035e6c85a43c0ceb7469c4f3f378d3
Fernando Brito <email@fernandobrito.com>
2017-03-07 14:41:47 -0300

    [python] Update solution to task 1.6

--- python/task1_1.py
+++ python/task1_1.py
@@ -9,8 +9,8 @@ import utils
 def main():
     image = utils.load_image('lenna.png')
 
-    yiq = _rgb2yiq(image)
-    back_to_original = _yiq2rgb(yiq)
+    yiq = utils.rgb2yiq(image)
+    back_to_original = utils.yiq2rgb(yiq)
 
     utils.display_single_image('Original', image)
     utils.display_single_image('YIQ', yiq)
@@ -19,45 +19,6 @@ def main():
     utils.save_image('yiq.png', yiq)
 
     utils.wait_key_and_destroy_windows()
-
-
-def _rgb2yiq(image):
-    output = np.empty_like(image)
-    blue, green, red = utils.split_channels(image)
-
-    # From https://www.mathworks.com/help/images/ref/rgb2ntsc.html
-    # Y = 0.299 R + 0.587 G + 0.114 B
-    # I = 0.596 R - 0.274 G - 0.322 B
-    # Q = 0.211 R - 0.523 G + 0.312 B
-
-    # TODO: Write on report about int8 overflowing negative
-    # values to positive
-
-    output[:, :, 2] = 0.299 * red + 0.587 * green + 0.114 * blue
-    output[:, :, 1] = 0.596 * red - 0.274 * green - 0.322 * blue
-    output[:, :, 0] = 0.211 * red - 0.523 * green + 0.312 * blue
-
-    utils.fit_matrix_in_interval(output)
-
-    return output
-
-
-def _yiq2rgb(image):
-    output = np.empty_like(image)
-    q, i, y = utils.split_channels(image)
-
-    # From https://www.mathworks.com/help/images/ref/ntsc2rgb.html
-    # R = 1 Y  + 0.956 I + 0.621 Q
-    # G = 1 Y  - 0.272 I - 0.647 Q
-    # B = 1 Y  - 1.106 I + 1.703 Q
-
-    output[:, :, 2] = y + 0.956 * i + 0.621 * q
-    output[:, :, 1] = y - 0.272 * i - 0.647 * q
-    output[:, :, 0] = y - 1.106 * i + 1.703 * q
-
-    utils.fit_matrix_in_interval(output)
-
-    return output
 
 
 if __name__ == "__main__":
--- python/task1_6.py
+++ python/task1_6.py
@@ -9,23 +9,37 @@ import color
 
 def main():
     image = utils.load_image('lenna.png')
-    utils.display_single_image('Original', image)
-    grayscale_image = color.rbg2gray(image)
 
-    threshold_value = 200
+    yiq_image = color.rgb2yiq(image)
+    grayscale_image = color.yiq2rgb(image)[:, :, 0]
+
+    threshold_value = 255 * 0.2
     mean_value = np.mean(grayscale_image)
-    
-    threshold_user_image = _segment_y(grayscale_image, threshold_value)
-    threshold_mean_image = _segment_y(grayscale_image, mean_value)
 
+    threshold_user_image = _segment(grayscale_image, threshold_value)
+    original_threshold_user_image = np.copy(yiq_image)
+    original_threshold_user_image[:, :, 0] = threshold_user_image
+    original_threshold_user_image = color.yiq2rgb(original_threshold_user_image)
+
+    threshold_mean_image = _segment(grayscale_image, mean_value)
+    original_threshold_mean_image = np.copy(yiq_image)
+    original_threshold_mean_image[:, :, 0] = threshold_mean_image
+    original_threshold_mean_image = color.yiq2rgb(original_threshold_mean_image)
+
+    utils.display_single_image('Original Image', image)
+    utils.display_single_image('YIQ Image', yiq_image)
     utils.display_single_image('Y Channel', grayscale_image)
+
     utils.display_single_image('Y Threshold (User ' + str(threshold_value) + ')', threshold_user_image)
+    utils.display_single_image('Back to Original (User ' + str(threshold_value) + ')', original_threshold_user_image)
+
     utils.display_single_image('Y Threshold (Mean ' + str(mean_value) + ')', threshold_mean_image)
+    utils.display_single_image('Back to Original (Mean ' + str(mean_value) + ')', original_threshold_mean_image)
 
     utils.wait_key_and_destroy_windows()
 
 
-def _segment_y(image, m):
+def _segment(image, m):
     output = (image > m) * 255
     return output
 
