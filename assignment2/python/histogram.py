96e38194ac2efe271aa402222d8da33450c2a5fe
Fernando Brito <email@fernandobrito.com>
2017-04-13 21:57:42 -0300

    Add histogram expansion

--- assignment2/python/histogram.py
+++ assignment2/python/histogram.py
@@ -15,14 +15,36 @@ def equalize(histogram, image):
     output = np.empty_like(image)
 
     total_pixels = image.shape[0] * image.shape[1]
-    max_intensity = 255  # L - 1
+    max_intensity = 255.0  # L - 1
 
     lut = np.empty_like(histogram)
     sum = 0
 
     for index, value in enumerate(histogram):
         sum += value
-        lut[index] = sum * max_intensity / total_pixels
+        lut[index] = round(sum * max_intensity / total_pixels)
+
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+            output[i, j] = lut[image[i, j]]
+
+    return output
+
+
+def expand(histogram, image):
+    output = np.empty_like(image)
+
+    max_intensity = 255.0  # L - 1
+
+    min = np.min(image)
+    max = np.max(image)
+
+    print("Expansion min, max: " + str(min) + ", " + str(max))
+
+    lut = np.empty_like(histogram)
+
+    for index, value in enumerate(histogram):
+        lut[index] = round((index - min) / float(max - min) * max_intensity)
 
     for i in range(0, image.shape[0]):
         for j in range(0, image.shape[1]):
--- assignment2/python/task1.py
+++ assignment2/python/task1.py
@@ -6,12 +6,16 @@ import histogram as hist
 import matplotlib.pyplot as plt
 
 def main():
-    image = utils.load_image('lenna.png')
+    image = utils.load_image('underexposed2.jpg')
+
     image = color.rbg2gray(image)
     histogram = hist.extract(image)
 
     equalized_image = hist.equalize(histogram, image)
     equalized_histogram = hist.extract(equalized_image)
+
+    expanded_image = hist.expand(histogram, image)
+    expanded_histogram = hist.extract(expanded_image)
 
     plt.figure(1)
     plt.bar(range(0, 256), histogram)
@@ -21,10 +25,15 @@ def main():
     plt.bar(range(0, 256), equalized_histogram)
     plt.title('Equalized Histogram')
 
+    plt.figure(3)
+    plt.bar(range(0, 256), expanded_histogram)
+    plt.title('Expanded Histogram')
+
     plt.show(block=False)
 
     utils.display_single_image('Original', image)
     utils.display_single_image('Equalized', equalized_image)
+    utils.display_single_image('Expanded', expanded_image)
 
     utils.wait_key_and_destroy_windows()
 
