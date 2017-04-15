07065c5acd6e8d44fda271e55ca1b6ec24d800f2
Italo Ayres <Italo Ayres>
2017-04-15 14:18:19 -0300

    [Matlab] Update plot layout on task #1

--- assignment2/matlab/task1.m
+++ assignment2/matlab/task1.m
@@ -2,7 +2,7 @@
 %   a. Equalização de histograma;
 %   b. Expansão de histograma.
 function task1()
-    assetsDir = ['..' filesep '..' filesep 'assets' filesep];
+    assetsDir = ['..' filesep 'assets' filesep];
     filePath = strcat(assetsDir, 'lenna.png');
     originalImage = imread(filePath);
         
@@ -14,16 +14,16 @@ function task1()
     histExp = imadjust(grayIm,stretchlim(grayIm),[]);
     
     % Ploting
-    fig = figure(1);
-    set (fig, 'Units', 'normalized', 'Position', [0,0,1,1]);
-    
-    subplot(2,3,1), imshow(grayIm), title('Original image')
-    subplot(2,3,4), imhist(grayIm), title('Image histogram')
-    subplot(2,3,2), imshow(histEq), title('Image Equalization')  
-    subplot(2,3,5), imhist(histEq), title('Histogram Equalization')     
-    subplot(2,3,3), imshow(histExp), title('Image Expansion')  
-    subplot(2,3,6), imhist(histExp), title('Histogram Expansion') 
+    figure();        
+    subplot(1,2,1), imshow(grayIm), title('Original image')
+    subplot(1,2,2), imhist(grayIm), title('Image histogram')
+    figure();
+    subplot(1,2,1), imshow(histEq), title('Image Equalization')  
+    subplot(1,2,2), imhist(histEq), title('Histogram Equalization')     
+    figure();
+    subplot(1,2,1), imshow(histExp), title('Image Expansion')  
+    subplot(1,2,2), imhist(histExp), title('Histogram Expansion') 
 
     
-    saveas(fig,strcat('output',filesep,mfilename,'.png'));
+    %saveas(fig,strcat('output',filesep,mfilename,'.png'));
 end
