2854292c3fe4ea92d98e544ecda722980de3aea1
Fernando Brito <email@fernandobrito.com>
2017-04-15 12:27:55 -0300

    Update task 1 solutions

--- assignment2/matlab/task1.m
+++ assignment2/matlab/task1.m
@@ -1,29 +1,32 @@
-% 1. Para uma imagem com baixo contraste, implemente as operações:
-%   a. Equalização de histograma;
-%   b. Expansão de histograma.
+% 1. Para uma imagem com baixo contraste, implemente as operaÃ§Ãµes:
+%   a. EqualizaÃ§Ã£o de histograma;
+%   b. ExpansÃ£o de histograma.
 function task1()
-    assetsDir = ['..' filesep '..' filesep 'assets' filesep];
+    assetsDir = ['..' filesep 'assets' filesep];
     filePath = strcat(assetsDir, 'lenna.png');
     originalImage = imread(filePath);
-        
-    yiqImage = rgb2ntsc(originalImage);
-    % Get grayscale image
-    grayIm = yiqImage(:,:,1);
-        
-    histEq = histeq(grayIm);    
+
+    [~, ~, channels] = size(originalImage);
+
+    if channels == 3
+        grayIm = rgb2gray(originalImage);
+    else 
+        grayIm = originalImage;
+    end
+
+    histEq = histeq(grayIm, 256);    
     histExp = imadjust(grayIm,stretchlim(grayIm),[]);
-    
+
     % Ploting
     fig = figure(1);
     set (fig, 'Units', 'normalized', 'Position', [0,0,1,1]);
-    
+
     subplot(2,3,1), imshow(grayIm), title('Original image')
     subplot(2,3,4), imhist(grayIm), title('Image histogram')
-    subplot(2,3,2), imshow(histEq), title('Image Equalization')  
-    subplot(2,3,5), imhist(histEq), title('Histogram Equalization')     
-    subplot(2,3,3), imshow(histExp), title('Image Expansion')  
-    subplot(2,3,6), imhist(histExp), title('Histogram Expansion') 
+    subplot(2,3,2), imshow(histEq), title('Image Equalization')
+    subplot(2,3,5), imhist(histEq), title('Histogram Equalization')
+    subplot(2,3,3), imshow(histExp), title('Image Expansion')
+    subplot(2,3,6), imhist(histExp), title('Histogram Expansion')
 
-    
-    saveas(fig,strcat('output',filesep,mfilename,'.png'));
+    % saveas(fig,strcat('output',filesep,mfilename,'.png'));
 end
